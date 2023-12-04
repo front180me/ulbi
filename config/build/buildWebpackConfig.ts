14338fa4fc6753d64fd3695f6112834510a63e50
Packman <front180me@gmail.com>
2023-12-04 19:36:39 +0300

    ad env variables for webpack config

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -7,10 +7,10 @@ import {buildPlugins} from "./buildPlugins";
 import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const { paths, mode } = options
+    const { paths, mode, isDev } = options
 
     return {
-        mode: 'development',
+        mode,
         entry: paths.entry,
         output: {
             filename: '[name].[contenthash].js',
@@ -22,7 +22,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         },
         resolve: buildResolvers(),
         plugins: buildPlugins(options),
-        devtool: 'inline-source-map',
-        devServer: buildDevServer(options),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
     }
 }

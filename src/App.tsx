c81879a7552ec4f10a3a81d505003cd553be25d8
Packman <front180me@gmail.com>
2023-12-07 22:33:02 +0300

    add theme context

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,11 @@ import {Link, Route, Routes} from 'react-router-dom';
 import './styles/index.scss'
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {Suspense, useState} from "react";
-
-export enum Theme {
-    LIGHT = 'light',
-    DARK = 'dark',
-}
+import {Suspense} from "react";
+import {useTheme} from "./theme/useTheme";
 
 const App = () => {
-    const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
-
-    const toggleTheme = () => {
-        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
-    }
-
+    const { theme, toggleTheme } = useTheme();
     return (
         <div className={`app ${theme}`}>
             <button onClick={toggleTheme}>TOGGLE</button>

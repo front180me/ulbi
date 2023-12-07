511a39bc722a6d7e397fc21b52f6a74d99585f98
Packman <front180me@gmail.com>
2023-12-07 21:43:38 +0300

    add style themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import {Link, Route, Routes} from 'react-router-dom';
-import './index.scss'
+import './styles/index.scss'
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {Suspense} from "react";
+import {Suspense, useState} from "react";
 
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark',
+}
 
 const App = () => {
+    const [theme, setTheme] = useState<Theme>(Theme.LIGHT)
+
+    const toggleTheme = () => {
+        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK)
+    }
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to="/">Main page</Link>
             <Link to="/about">About</Link>
             <Suspense fallback={<div>Loading...</div>}>

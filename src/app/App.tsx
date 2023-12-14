b0db86ee96d2c1a8e1fc89945ae12e5d25633fc4
Packman <front180me@gmail.com>
2023-12-14 21:31:47 +0300

    refactor router components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,19 @@
-import {Link, Route, Routes} from 'react-router-dom';
+import {Link, Route, Routes} from 'react-router-dom'
 import './styles/index.scss'
-import {classNames} from "shared/lib/classNames/classNames";
-import {useTheme} from "app/providers/ThemeProvider";
-import {AppRouter} from "app/providers/router";
+import {classNames} from "shared/lib/classNames/classNames"
+import {useTheme} from "app/providers/ThemeProvider"
+import {AppRouter} from "app/providers/router"
+import {Navbar} from "widgets/Navbar"
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to="/">Main page</Link>
-            <Link to="/about">About</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
 
-export default App;
+export default App

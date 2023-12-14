c0c36e85f81a9ee51ce634664e1d035f8bd08c9f
Packman <front180me@gmail.com>
2023-12-14 22:37:07 +0300

    setup file loader, add Button component, assets folder, update theme switcher

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import {classNames} from "shared/lib/classNames/classNames"
 import cls from './Navbar.module.scss'
 import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink"
+import {ThemeSwitcher} from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
     className?: string
@@ -8,6 +9,7 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
+            <ThemeSwitcher className={'sdsds'}/>
             <div className={cls.links}>
                 <AppLink to="/" theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>Main page</AppLink>
                 <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>About</AppLink>

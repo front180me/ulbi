255cd6ffa84f3955175768141c92014d652b2add
Packman <front180me@gmail.com>
2023-12-21 22:22:48 +0300

    add i18next

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from 'react'
+import {useTranslation} from "react-i18next";
+
 
 const MainPage = () => {
+    const {t} = useTranslation('main')
     return (
         <div>
-            MAIN PAGE
+            {t('Главная страница')}
         </div>
-    );
-};
+    )
+}
 
-export default MainPage;
+export default MainPage
